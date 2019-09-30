8893a79f3cbc3d555160fc8051e90bdd402b3150
lynx <1197225628@qq.com>
2019-09-30 10:36:03 +0800

    getcwd

--- inspector.c
+++ inspector.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
 #include <assert.h>
-#include <stdlib.h>
 #include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define GREEN(format, ...) \
   printf("\033[1;32m" format "\33[0m\n", ## __VA_ARGS__)
 
-int main()
+char root_path[128];// 监视的文件根目录
+
+int main(int argc, char *argv[])
 {
+  if (argc <= 1) {// 没有参数
+    getcwd(root_path, sizeof(root_path));
+  } else {
+    sprintf(root_path, "%s", argv[1]);
+  }
+  GREEN("%s", root_path);
   return 0;
 }
