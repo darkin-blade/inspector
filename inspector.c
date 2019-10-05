c25c1699b8342c241e9eae08ed7ff25cf39bb770
lynx <1197225628@qq.com>
2019-10-05 09:16:36 +0800

    列举所有event

--- inspector.c
+++ inspector.c
@@ -36,7 +36,7 @@ void test()
   assert(fd >= 0);
 
   char buf[EVENT_BUF_LEN];
-  int watch_id = inotify_add_watch(fd, root_path, IN_CREATE | IN_DELETE);// TODO
+  int watch_id = inotify_add_watch(fd, root_path, IN_CREATE | IN_DELETE);// 对指定路径进行监视
 
   while (1) {
     int length = read(fd, buf, EVENT_BUF_LEN);
@@ -45,12 +45,28 @@ void test()
     for (int i = 0; i < length;) {
       struct inotify_event *event = (struct inotify_event *)&buf[i];
       if (event->len) {
-        if (event->mask & IN_CREATE) {
+        if (event->mask & IN_ACCESS) {
+          GREEN("access");
+        } else if (event->mask & IN_ATTRIB) {
+          GREEN("attrib");
+        } else if (event->mask & IN_CLOSE_WRITE) {
+          GREEN("close write");
+        } else if (event->mask & IN_CLOSE_NOWRITE) {
+          GREEN("close no write");
+        } else if (event->mask & IN_CREATE) {
           GREEN("create");
         } else if (event->mask & IN_DELETE) {
           GREEN("delete");
+        } else if (event->mask & IN_DELETE_SELF) {
+          GREEN("delete self");
         } else if (event->mask & IN_MODIFY) {
           GREEN("modity");
+        } else if (event->mask & IN_MOVE_SELF) {
+          GREEN("move self");
+        } else if (event->mask & IN_MOVED_FROM) {
+          GREEN("move from");
+        } else if (event->mask & IN_MOVED_TO) {
+          GREEN("move to");
         }
       }
       i += EVENT_SIZE + event->len;
